ea5905b01e96b634e55033a5b0ee0b63b74617e7
Roey Borovsky <95938066+Rb49@users.noreply.github.com>
2024-08-24 00:26:13 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+if __name__ == '__main__':
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = MyClient(intents=intents)
+
+    with open('TOKEN', 'r') as f:
+        token = f.read()
+
+    client.run(token)
+
+
