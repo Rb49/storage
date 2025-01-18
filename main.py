390f2915e94f2d3f015dc6ad77095fdc769e8067
Roey Borovsky <95938066+Rb49@users.noreply.github.com>
2025-01-18 20:44:50 +0200

    commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 
 
+from file import send_segments, File
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -11,17 +14,24 @@ class MyClient(discord.Client):
             return
 
         if message.content == 'ping':
+            # Example byte array (binary data)
+            # path = r"C:\Users\roeyb\Downloads\balenaEtcher-portable.exe"
+            path = r"C:\Users\roeyb\Downloads\25MB_file.bin"
+            d = await send_segments(message, path)
+            print(d)
+
             await message.channel.send('pong')
 
 
 if __name__ == '__main__':
+    import tracemalloc
+    tracemalloc.start()
+
     intents = discord.Intents.default()
     intents.message_content = True
     client = MyClient(intents=intents)
 
-    with open('TOKEN', 'r') as f:
+    with open('ENV', 'r') as f:
         token = f.read()
 
     client.run(token)
-
-
